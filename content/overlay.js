5cb941bc0c6a6711e82d62d8490ca913c51ee78f
Lucas M. Bonomo <lucas@vgnet.com.ar>
2019-10-15 17:27:45 -0300

    Creo... que es la primera version estable

--- content/overlay.js
+++ content/overlay.js
@@ -1,6 +1,6 @@
 var gsuiteintab = gsuiteintab || {};
 
-gsuiteintab.ShowCalendar = function(e) {
+gsuiteintab.ShowCalendar = function(event) {
       var aURL = "https://calendar.google.com/calendar/r";
       document.getElementById('tabmail').openTab(
         "contentTab",
@@ -11,7 +11,7 @@ gsuiteintab.ShowCalendar = function(e) {
 };
 
 
-gsuiteintab.ShowKeep = function(e) {
+gsuiteintab.ShowKeep = function(event) {
       var aURL = "https://keep.google.com/";
       document.getElementById('tabmail').openTab(
         "contentTab",
